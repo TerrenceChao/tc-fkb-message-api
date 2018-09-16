8d51c78226700469d291c89e125769f2c4b5c11b
TerrenceChao <rtyufjvbn@gmail.com>
2018-09-16 12:19:59 +0800

    code style

--- src/service/storageService.js
+++ src/service/storageService.js
@@ -6,7 +6,7 @@ const {
   repository
 } = require(path.join(config.get('database'), 'repository'))
 
-function StorageService() {}
+function StorageService () {}
 
 StorageService.prototype.getChannelInfoIds = async function (uid, limit = 'all', skip = 0) {
   return ['chPub', 'chGasStation', 'chHospital']
