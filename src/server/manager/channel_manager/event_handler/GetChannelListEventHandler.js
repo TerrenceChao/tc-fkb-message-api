d9a503bf941bf74c255a8062a41b8fee9552ef59
TerrenceChao <rtyufjvbn@gmail.com>
2018-10-03 23:58:30 +0800

    add function => get channel list

--- src/server/manager/channel_manager/event_handler/GetChannelListEventHandler.js
+++ src/server/manager/channel_manager/event_handler/GetChannelListEventHandler.js
@@ -3,10 +3,12 @@ var util = require('util')
 var path = require('path')
 
 const {
-  EVENTS
+  TO,
+  EVENTS,
+  RESPONSE_EVENTS
 } = require(path.join(config.get('property'), 'property'))
+const ResponseInfo = require(path.join(config.get('manager'), 'ResponseInfo'))
 const EventHandler = require(path.join(config.get('manager'), 'EventHandler'))
-const RequestInfo = require(path.join(config.get('manager'), 'RequestInfo'))
 
 util.inherits(GetChannelListEventHandler, EventHandler)
 
@@ -17,7 +19,36 @@ function GetChannelListEventHandler () {
 GetChannelListEventHandler.prototype.eventName = EVENTS.GET_CHANNEL_LIST
 
 GetChannelListEventHandler.prototype.handle = async function (requestInfo) {
+  if (!this.isValid(requestInfo)) {
+    console.warn(`${this.eventName}:`, `request info is invalid.`)
+    return
+  }
 
+  var packet = requestInfo.packet
+  var uid = packet.uid
+
+  var businessEvent = this.globalContext['businessEvent']
+  var storageService = this.globalContext['storageService']
+  var userChannelInfo = await storageService.getUserChannelInfo(uid)
+
+  var resInfo = new ResponseInfo()
+    .assignProtocol(requestInfo)
+    .setHeader({
+      to: TO.USER,
+      receiver: uid,
+      responseEvent: RESPONSE_EVENTS.CHANNEL_LIST
+    })
+    .setPacket({
+      msgCode: `channel list`,
+      data: userChannelInfo
+    })
+  businessEvent.emit(EVENTS.SEND_MESSAGE, resInfo)
+}
+
+GetChannelListEventHandler.prototype.isValid = function (requestInfo) {
+  var packet = requestInfo.packet
+  return packet !== undefined && this.isAuthenticated(packet) &&
+    typeof packet.uid === 'string'
 }
 
 module.exports = {
