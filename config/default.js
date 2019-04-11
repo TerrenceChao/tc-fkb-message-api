a22613cb80db189e63be289be71a5f3268b94101
TerrenceChao <rtyufjvbn@gmail.com>
2019-04-11 17:28:25 +0800

    update config(default) file

--- config/default.js
+++ config/default.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 var fs = require('fs')
 var path = require('path')
-var package = JSON.parse(fs.readFileSync(__dirname + '/../package.json'))
-const ROOT = getParentDirPath(__dirname, package.name)
+var project = JSON.parse(fs.readFileSync('package.json'))
+const ROOT = getParentDirPath(__dirname, project.name)
 
 /**
  * @private
@@ -9,26 +10,21 @@ const ROOT = getParentDirPath(__dirname, package.name)
  * @param {string} specifyDir
  * @returns {string}
  */
-function getParentDirPath(currentDir, specifyDir) {
+function getParentDirPath (currentDir, specifyDir) {
   if (specifyDir == null) {
     return path.resolve(currentDir, '../')
   }
 
-  while (path.basename(currentDir) != specifyDir) {
+  while (path.basename(currentDir) !== specifyDir) {
     currentDir = path.resolve(currentDir, '../')
   }
   return currentDir
 }
 
-require('dotenv').config({
-  path: path.join(ROOT, '.env')
-})
-
 module.exports = {
-
   app: require('./_app'),
   auth: require('./_auth'),
   adaptor: require('./_adaptor'),
   src: require('./_src')(ROOT),
-  test: require('./_test')(ROOT),
+  test: require('./_test')(ROOT)
 }
