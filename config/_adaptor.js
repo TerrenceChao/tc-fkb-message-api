1f92c1641fe4045dd9b0c0c69b5372d8aad6515b
TerrenceChao <rtyufjvbn@gmail.com>
2019-04-11 23:20:08 +0800

    fix: remove duplicated require

--- config/_adaptor.js
+++ config/_adaptor.js
@@ -1,5 +1,3 @@
-require('dotenv').config()
-
 module.exports = {
   host: process.env.ADAPTOR_HOST,
   port: process.env.ADAPTOR_PORT
--- config/_app.js
+++ config/_app.js
@@ -1,5 +1,3 @@
-require('dotenv').config()
-
 module.exports = {
   host: process.env.SERVER_HOST,
   port: process.env.SERVER_PORT
