b4e91e3023b565b1c353d9cab634c00e4dc9943e
TerrenceChao <rtyufjvbn@gmail.com>
2019-05-05 23:12:46 +0800

    fix: improve database config

--- config/_database.js
+++ config/_database.js
@@ -2,43 +2,44 @@ const path = require('path')
 const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
-
-var writeConcern = {
-  w: process.env.MONGODB_WRITE_CONCERN_W,
-  j: JSON.parse(process.env.MONGODB_WRITE_CONCERN_J),
-  wtimeout: process.env.MONGODB_WRITE_CONCERN_WTIMEOUT
-}
-
 mongoose.envParams = {
-  writeConcern
+  writeConcern: {
+    w: process.env.MONGODB_WRITE_CONCERN_W || 'majority',
+    j: JSON.parse(process.env.MONGODB_WRITE_CONCERN_J || true),
+    wtimeout: process.env.MONGODB_WRITE_CONCERN_WTIMEOUT || 1000
+  }
 }
 
-const HOST = process.env.MONGODB_HOST
-const POOL_SIZE = process.env.MONGODB_POOL_SIZE
-const CONNECT = 'req_connect'
-const DISCONNECT = 'req_disconnect'
+function NosqlShell () {
+  mongoose.connection.on('error', console.error.bind(console, 'connection error:'))
+  mongoose.connection.once('open', () => {
+    console.log('mongodb is connecting ...')
+  })
 
-mongoose.connection.once(CONNECT, (host = null) => {
-  host = (host === null) ? HOST : host
+  mongoose.connection.once('disconnected', () => {
+    console.log('mongodb is disconnected')
+  })
+}
+
+NosqlShell.prototype.db = function () {
+  return mongoose
+}
+
+NosqlShell.prototype.connect = function (host = null) {
+  host = host || process.env.MONGODB_HOST
   mongoose.connect(host, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    autoIndex: false
+    autoIndex: false,
+    reconnectTries: Number.MAX_VALUE,
+    reconnectInterval: process.env.MONGODB_RECONNECT_INTERVAL || 500,
+    poolSize: process.env.MONGODB_POOL_SIZE || 10
   })
-})
+}
 
-mongoose.connection.once(DISCONNECT, () => {
+NosqlShell.prototype.disconnect = function () {
   mongoose.disconnect()
-})
-
-mongoose.connection.on('error', console.error.bind(console, 'connection error:'))
-mongoose.connection.once('open', () => {
-  console.log('mongodb is connecting ...')
-})
-
-mongoose.connection.once('disconnected', () => {
-  console.log('mongodb is disconnected')
-})
+}
 
 module.exports = function (root) {
   var database = path.join(root, 'infrastructure', 'database')
@@ -47,18 +48,15 @@ module.exports = function (root) {
 
   return {
     nosql: {
-      host: HOST,
-      poolSize: POOL_SIZE,
-      connect: CONNECT,
-      disconnect: DISCONNECT,
+      db: new NosqlShell(),
       factory: path.join(nosql, 'factory'),
       model: path.join(nosql, 'model'),
-      Seed: path.join(nosql, 'seed')
+      seed: path.join(nosql, 'seed')
     },
     sql: {
       factory: path.join(sql, 'factory'),
       model: path.join(sql, 'model'),
-      Seed: path.join(sql, 'seed')
+      seed: path.join(sql, 'seed')
     }
   }
 }
