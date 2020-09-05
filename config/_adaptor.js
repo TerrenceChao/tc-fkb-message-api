1ff3627184ab6ff19094819878b9a925b99330ed
TerrenceChao <rtyufjvbn@gmail.com>
2020-09-05 16:49:45 +0800

    feat: password required for apaapter(redis)

--- config/_adaptor.js
+++ config/_adaptor.js
@@ -1,4 +1,5 @@
 module.exports = {
   host: process.env.ADAPTOR_HOST,
-  port: process.env.ADAPTOR_PORT
+  port: process.env.ADAPTOR_PORT,
+  password: process.env.ADAPTOR_PASSWORD
 }
--- src/server/Adapter.js
+++ src/server/Adapter.js
@@ -1,11 +1,15 @@
 const config = require('config')
+const redis = require('redis')
 const redisAdaptor = require('socket.io-redis')
+const host = config.get('adaptor.host')
+const port = config.get('adaptor.port')
+const password = config.get('adaptor.password')
+
+const pub = redis.createClient(port, host, { auth_pass: password })
+const sub = redis.createClient(port, host, { auth_pass: password })
 
 function adaptor (socketServer) {
-  socketServer.adapter(redisAdaptor({
-    host: config.get('adaptor.host'),
-    port: config.get('adaptor.port')
-  }))
+  socketServer.adapter(redisAdaptor({ pubClient: pub, subClient: sub }))
 }
 
 module.exports = {
